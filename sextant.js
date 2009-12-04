97e2f8db26a35b2712ab396329b3ba3bea550f1c
Tim Trefren <ttrefren@gmail.com>
2009-12-03 17:25:15 -0700

    Working on URL regex handler

--- sextant.js
+++ sextant.js
@@ -1,7 +1,7 @@
 function Sextant(interval) {
     if (!interval) { interval = 100 }
     this.urlpatterns = [];
-    
+    this.DEBUG = true;
     this.run(interval)
 }
 
@@ -13,15 +13,44 @@ Sextant.prototype.UrlPatterns = function(patterns) {
     this.urlpatterns = this.urlpatterns.concat(patterns);
 }
 
+Sextant.prototype.UrlParser = function(hash) {
+/*
+    Parse a hash URL into three parts.
+    Ex: #/users/john/doe?name=eric&size=2
+    
+    url.base = '#/users/john/doe'
+    url.paramstring = 'name=eric&size=2'
+    url.params = { 'name': 'eric', 'size': '2' }
+*/
+    var url = {};
+    var split = hash.split("?");
+    url.base = split[0];
+    url.paramstring = split[1]
+    if (split[1].length) {
+        var params = split[1].split("&");
+        var h = '';
+        for (var p in params) {
+            h = params[p].split("=");
+            url.params[h[0]] = h[1];
+        }
+    }
+    if (split.length > 2 && this.DEBUG == true) {
+        alert("Too many ? in hash: " + hash);
+    }
+    return url;
+}
+
 Sextant.prototype.UrlHandler = function(hash) {
+    
     console.log(hash);
+    var parsed = this.UrlParser(hash);
+    console.dir(parsed);
+    
     console.dir(this.urlpatterns);
     for (var i in this.urlpatterns) {
         var p = this.urlpatterns[i];
-        console.log("testing..");
-        console.log(p[0]);
-        console.log(hash);
-        if (p[0].test(hash)) {
+        if (p[0].test(parsed.url)) {
+            var matches = p[0].exec(parsed.url);
             p[1].display();
             break;
         } else {
